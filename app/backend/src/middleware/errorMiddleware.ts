c0f8ebacfc20ad8916f5b477c3565d4bc6901db8
Joao <me.contrate.aero@gmail.com>
2022-10-31 13:45:04 -0300

    corrigido status de erro no erro midleware

--- app/backend/src/middleware/errorMiddleware.ts
+++ app/backend/src/middleware/errorMiddleware.ts
@@ -5,7 +5,7 @@ const errorMiddleware: ErrorRequestHandler = (err, req, res, _next) => {
     return res.status(err.status).send({ message: err.message });
   }
   if (err.message === 'invalid signature' || err.message === 'jwt expired') {
-    return res.status(401).send({ message: 'Token must be a valid token' });
+    return res.status(404).send({ message: 'Token must be a valid token' });
   }
   return res.status(500).send({ message: err.message });
 };
