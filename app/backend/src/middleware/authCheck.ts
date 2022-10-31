27a7a25ce8581db714ed8fe8e34e80c12b2fa4e8
Joao <me.contrate.aero@gmail.com>
2022-10-31 14:48:02 -0300

    implementação dos point match/id e match/id/finish e correção no midleware de erro

--- app/backend/src/middleware/authCheck.ts
+++ app/backend/src/middleware/authCheck.ts
@@ -5,7 +5,7 @@ import StructuredError from '../errors/StructuredError';
 const authCheck = (req: Request, res: Response, next: NextFunction) => {
   const token: string | any = req.headers.authorization;
   if (!token) {
-    throw new StructuredError('Token not found', 401);
+    throw new StructuredError('Token must be a valid token', 401);
   }
   const user = tokenManager.verifyToken(token);
   if (!user) {
--- app/backend/src/middleware/errorMiddleware.ts
+++ app/backend/src/middleware/errorMiddleware.ts
@@ -4,8 +4,12 @@ const errorMiddleware: ErrorRequestHandler = (err, req, res, _next) => {
   if (err.status) {
     return res.status(err.status).send({ message: err.message });
   }
-  if (err.message === 'invalid signature' || err.message === 'jwt expired') {
-    return res.status(404).send({ message: 'Token must be a valid token' });
+  if (err.message === 'invalid signature'
+   || err.message === 'jwt expired'
+   || err.message === 'jwt malformed'
+   || err.message === 'invalid token'
+  ) {
+    return res.status(401).send({ message: 'Token must be a valid token' });
   }
   return res.status(500).send({ message: err.message });
 };
